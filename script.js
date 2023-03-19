9325aa05803ab8e1ebc320e0bdd316267843923a
unnecessaryEvil <waziliys@gmail.com>
2023-03-20 02:09:59 +0300

    fix header logic on mobile

--- script.js
+++ script.js
@@ -1,19 +1,31 @@
 var header = document.querySelector(".sticky-header");
-var headerHeight = header.offsetHeight;
+var logoHeader = document.querySelector(".logo");
+var linksHeader = document.querySelector(".links");
+var linksPlaceholder = document.querySelector(".links-placeholder");
+var linksHeaderTop = linksHeader.offsetTop;
 var prevScrollpos = window.pageYOffset;
+var isMobile = window.matchMedia("(max-width: 767px)").matches;
 
-window.onscroll = function() {
-  var currentScrollPos = window.pageYOffset;
-  if (prevScrollpos > currentScrollPos) {
-    header.classList.add("visible");
-    header.classList.remove("docked");
-  } else {
-    if (currentScrollPos >= headerHeight) {
-      header.classList.add("docked");
+if (!isMobile) {
+  window.onscroll = function() {
+    var currentScrollPos = window.pageYOffset;
+    if (prevScrollpos > currentScrollPos) {
+      header.classList.add("visible");
+      logoHeader.classList.remove("docked");
+      linksHeader.classList.remove("fixed");
+      linksPlaceholder.style.height = "0";
     } else {
-      header.classList.remove("docked");
+      if (currentScrollPos >= linksHeaderTop) {
+        logoHeader.classList.add("docked");
+        linksHeader.classList.add("fixed");
+        linksPlaceholder.style.height = linksHeader.offsetHeight + "px";
+      } else {
+        logoHeader.classList.remove("docked");
+        linksHeader.classList.remove("fixed");
+        linksPlaceholder.style.height = "0";
+      }
+      header.classList.remove("visible");
     }
-    header.classList.remove("visible");
-  }
-  prevScrollpos = currentScrollPos;
-};
+    prevScrollpos = currentScrollPos;
+  };
+}

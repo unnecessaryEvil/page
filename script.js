4c8eb8cdfb0623d5030ca4d0818f3f4c75e1cfa3
unnecessaryEvil <waziliys@gmail.com>
2023-03-20 04:56:20 +0300

    refactor js

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-var header = document.querySelector(".sticky-header");
-var logoHeader = document.querySelector(".logo");
-var linksHeader = document.querySelector(".links");
-var linksPlaceholder = document.querySelector(".links-placeholder");
-var linksHeaderTop = linksHeader.offsetTop;
-var prevScrollpos = window.pageYOffset;
-var isMobile = window.matchMedia("(max-width: 767px)").matches;
+const header = document.querySelector(".sticky-header");
+const logoHeader = document.querySelector(".logo");
+const linksHeader = document.querySelector(".links");
+const linksPlaceholder = document.querySelector(".links-placeholder");
+const linksHeaderTop = linksHeader.offsetTop;
+let prevScrollpos = window.pageYOffset;
+const isMobile = window.matchMedia("(max-width: 767px)").matches;
 
 if (!isMobile) {
   window.onscroll = function() {
-    var currentScrollPos = window.pageYOffset;
+    const currentScrollPos = window.pageYOffset;
     if (prevScrollpos > currentScrollPos) {
       header.classList.add("visible");
       logoHeader.classList.remove("docked");
@@ -31,33 +31,19 @@ if (!isMobile) {
 }
 
 window.onload = function() {
-
-  // Get all links with class "header-link"
-  var links = document.getElementsByClassName("header-link");
-
-  // Loop through each link
-  for (var i = 0; i < links.length; i++) {
-
-    // Add a click event listener to the link
+  const links = document.getElementsByClassName("header-link");
+  for (let i = 0; i < links.length; i++) {
     links[i].addEventListener("click", function(event) {
-
-      // Prevent the link from opening a new page
       event.preventDefault();
-
-      // Get the target div's ID from the link's href attribute
-      var targetID = this.getAttribute("href").substr(1);
-
-      // Scroll to the target div using a smooth animation
+      const targetID = this.getAttribute("href").substr(1);
       document.getElementById(targetID).scrollIntoView({
         behavior: "smooth"
       });
-      if(!isMobile){
-      setTimeout(function() {
-        header.classList.remove("visible");
-      }, 700);
-    };
+      if (!isMobile) {
+        setTimeout(function() {
+          header.classList.remove("visible");
+        }, 700);
+      }
     });
-
   }
-
 };
